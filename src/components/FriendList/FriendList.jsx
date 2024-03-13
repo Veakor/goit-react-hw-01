8398b281df73e8f45679ec95d1cf6f8696ba8927
Veakor <135655247+Veakor@users.noreply.github.com>
2024-03-13 19:53:08 +0100

    add

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,14 +1,10 @@
-/* eslint-disable react/prop-types */
-// eslint-disable-next-line no-unused-vars
-import styles from './FriendList.css'; 
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-import FriendListItem from './FriendList';
 
-// eslint-disable-next-line react/prop-types
+
+import styles from './FriendList.css';
+
 const FriendList = ({ friends }) => {
   return (
-    // eslint-disable-next-line react/jsx-no-comment-textnodes
+    
     <ul>
       {friends.map(({ id, ...props }) => (
         <li key={id}>
