5ce4ca485f21ab81535ae8d8a12640d9f402df60
Veakor <135655247+Veakor@users.noreply.github.com>
2024-03-13 12:09:24 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Profile from './components/Profile/Profile'; 
 import userData from './userData.json'; 
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+
 import styles from './Profile/Profile.module.css'; 
 
 // eslint-disable-next-line react/prop-types
