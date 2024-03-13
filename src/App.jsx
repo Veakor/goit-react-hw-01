9d21df4b6c519214fa84d8c68b0bdd3333b60a3f
Veakor <135655247+Veakor@users.noreply.github.com>
2024-03-13 20:13:10 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 export default App;
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList'; 
+// eslint-disable-next-line no-unused-vars
+import FriendListItem from './components/FriendListItem/FriendListItem'; 
 import userData from './userData.json'; 
 import friends from "./friends.json";
 
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+
 // eslint-disable-next-line no-unused-vars
 import styles from './FriendListItem.css'; 
 
